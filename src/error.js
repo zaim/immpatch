b864f998e7dd9b7717337a1d515ba1ae743d6b44
motiz88 <motiz88@gmail.com>
2015-06-10 18:53:12 +0300

    Fix Babel 5.x build and support error stack traces

--- src/error.js
+++ src/error.js
@@ -1,52 +1,56 @@
-export class PatchError extends Error {}
+export class PatchError extends Error {
+  // Based on https://gist.github.com/daliwali/09ca19032ab192524dc6
+  constructor (message) {
+    super();
+ 
+    if (Error.hasOwnProperty('captureStackTrace'))
+      Error.captureStackTrace(this, this.constructor);
+    else
+      Object.defineProperty(this, 'stack', {
+        value: (new Error()).stack
+      });
+ 
+    Object.defineProperty(this, 'message', {
+      value: message
+    });
+  }
+ 
+  get name () {
+    return this.constructor.name;
+  }
+}
 
 
 export class InvalidOperationError extends PatchError {
   constructor (op, reason) {
-    this.name = 'InvalidOperationError'
+    super(`Invalid ${op} operation: ${reason}`)
     this.op = op
     this.reason = reason
-  }
-
-  toString () {
-    return `Invalid ${this.op} operation: ${this.reason}`
   }
 }
 
 
 export class PathNotFoundError extends PatchError {
   constructor (path) {
-    this.name = 'PathNotFoundError'
+    super(`Path not found: ${path}`)
     this.path = path
-  }
-
-  toString () {
-    return `Path not found: ${this.path}`
   }
 }
 
 
 export class TestFailError extends PatchError {
   constructor (path, expected, value) {
-    this.name = 'TestFailError'
+    super(`Test failed: expected ${path} to be ${expected}, but got ${value}`)
     this.path = path
     this.expected = expected
     this.value = value
-  }
-
-  toString () {
-    return `Test failed: `
-    + `expected ${this.path} `
-    + `to be ${this.expected}, `
-    + `but got ${this.value}`
   }
 }
 
 
 export class PointerError extends PatchError {
   constructor (message) {
-    this.name = 'PointerError'
-    this.message = message
+    super(message)
   }
 }
 
